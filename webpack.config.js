db6a7d4d66f5e914a8c11b0dea14b767a6ebf914
sretundijr <sretundijr@gmail.com>
2018-01-26 14:26:26 -0700

    added source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 var path = require("path");
 
 var DIST_DIR = path.join(__dirname, "dist"),
-    CLIENT_DIR = path.join(__dirname, "src");
+	CLIENT_DIR = path.join(__dirname, "src");
 
 module.exports = {
-    context: CLIENT_DIR,
+	context: CLIENT_DIR,
 
-    entry: "./main",
+	entry: "./main",
 
-    output: {
-        path: DIST_DIR,
-        filename: "bundle.js"
-    },
+	output: {
+		path: DIST_DIR,
+		filename: "bundle.js",
+	},
+	devtool: 'source-map',
 
-    resolve: {
-        extensions: ['.js', '.jsx']
-    }
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
 };
